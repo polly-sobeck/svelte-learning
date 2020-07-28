2e80e122efbfae909b18eea2c5bd5f1231f73b5f
polly <sobeckp@oregonstate.edu>
2020-07-27 19:56:55 -0700

    Initial set-up for svelte project

---
+++ public/build/bundle.js
@@ -0,0 +1,6 @@
+var hello = () => {
+console.log('hello!');
+};
+
+hello();
+//# sourceMappingURL=bundle.js.map
---
+++ rollup.config.js
@@ -0,0 +1,8 @@
+export default {
+	input: 'src/main.js',
+	output: {
+		file: 'public/build/bundle.js',
+		format: 'esm',
+		sourcemap: true
+	}
+};
